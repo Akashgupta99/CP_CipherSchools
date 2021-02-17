781bae2f29c5cd87438f8120f5ea87dcb628fa5d
Akash Gupta <akashgupta2415@gmail.com>
2021-02-17 23:02:42 +0530

    Created program to find factorial of a number.

---
+++ Assignment2/Factorial.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n)
+{
+    if (n>1){ 
+        return n*fib(n-1);
+    }
+    else{
+        return 1;
+    }
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fib(n);
+    return 0;
+}
